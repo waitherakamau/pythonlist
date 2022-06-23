5cd90e0dcabb455e546fc5ffaba6b2fc1eefacbe
waitherakamau <effencekamau@gmail.com>
2022-06-23 19:35:44 +0300

    added triings.py

--- strings.py
+++ strings.py
@@ -1,3 +1,47 @@
+from operator import length_hint
+
+
+course="python programing"
+print(len(course))
+print(course[0])
+print(course[-1])
+
+#slice the string
+print(course[0:3])
+print(course[0:])
+print(course[:3])
+
+
+chapter="python\nprograming"
+print(chapter)
+
+cars="i love Audi.I enjoy driving Audi"
+# replace Audi with jeep
+new_text=cars.replace("Audi","jeep")
+print(new_text)
+
+# /n prints a newline
+workers="job \n jobless"
+print(workers)
+
+# Given two variables first an last print variable in full
+first="Effence"
+last="Kamau"
+full=first +""+ last
+print(full)
+# format 
+first="Effence"
+last="Kamau"
+full=f"{first} {last}"
+print(full)
+# get the length
+first="Effence"
+last="Kamau"
+full=f"{len(first)} {last}"
+
+# slicing
+s="cinema"
+s[2:5]
 
 
 
